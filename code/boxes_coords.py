6600c2b6797fe791e314afa91dd60d104d7c1c3f
steben_op <kostya.battlefield13@mail.ru>
2023-03-14 14:46:47 +0400

    [ref] --moved variables to class

--- code/boxes_coords.py
+++ code/boxes_coords.py
@@ -18,53 +18,53 @@ def load_boxes_coords(coord_x1, coord_y1, coord_x4, coord_y4, human_id, boxes_co
 def locating_inside_stop(stop_id, time, boxes_coords, stop_points):
     human_inside_count = 0
     # Проверка на нахождение точек ног внутри остановки
-    for i in range(len(boxes_coords)):
+    for human_id in range(len(boxes_coords)):
         if (  # Для xy первой координаты ног
-        ((boxes_coords[i][4] <= stop_points.getX1() and boxes_coords[i][4] <= stop_points.getX2() and
-          boxes_coords[i][4] <= stop_points.getX3() and boxes_coords[i][4] >= stop_points.getX4() and
-          boxes_coords[i][5] >= stop_points.getY1() and boxes_coords[i][5] >= stop_points.getY2() and
-          boxes_coords[i][5] <= stop_points.getY3() and boxes_coords[i][5] <= stop_points.getY4())
+        ((boxes_coords[human_id][4] <= stop_points.getX1() and boxes_coords[human_id][4] <= stop_points.getX2() and
+          boxes_coords[human_id][4] <= stop_points.getX3() and boxes_coords[human_id][4] >= stop_points.getX4() and
+          boxes_coords[human_id][5] >= stop_points.getY1() and boxes_coords[human_id][5] >= stop_points.getY2() and
+          boxes_coords[human_id][5] <= stop_points.getY3() and boxes_coords[human_id][5] <= stop_points.getY4())
          or
-         (boxes_coords[i][4] >= stop_points.getX1() and boxes_coords[i][4] <= stop_points.getX2() and
-          boxes_coords[i][4] >= stop_points.getX3() and boxes_coords[i][4] >= stop_points.getX4() and
-          boxes_coords[i][5] >= stop_points.getY1() and boxes_coords[i][5] >= stop_points.getY2() and
-          boxes_coords[i][5] <= stop_points.getY3() and boxes_coords[i][5] <= stop_points.getY4())
+         (boxes_coords[human_id][4] >= stop_points.getX1() and boxes_coords[human_id][4] <= stop_points.getX2() and
+          boxes_coords[human_id][4] >= stop_points.getX3() and boxes_coords[human_id][4] >= stop_points.getX4() and
+          boxes_coords[human_id][5] >= stop_points.getY1() and boxes_coords[human_id][5] >= stop_points.getY2() and
+          boxes_coords[human_id][5] <= stop_points.getY3() and boxes_coords[human_id][5] <= stop_points.getY4())
          or
-         (boxes_coords[i][4] >= stop_points.getX1() and boxes_coords[i][4] <= stop_points.getX2() and
-          boxes_coords[i][4] >= stop_points.getX3() and boxes_coords[i][4] <= stop_points.getX4() and
-          boxes_coords[i][5] >= stop_points.getY1() and boxes_coords[i][5] >= stop_points.getY2() and
-          boxes_coords[i][5] <= stop_points.getY3() and boxes_coords[i][5] <= stop_points.getY4())
+         (boxes_coords[human_id][4] >= stop_points.getX1() and boxes_coords[human_id][4] <= stop_points.getX2() and
+          boxes_coords[human_id][4] >= stop_points.getX3() and boxes_coords[human_id][4] <= stop_points.getX4() and
+          boxes_coords[human_id][5] >= stop_points.getY1() and boxes_coords[human_id][5] >= stop_points.getY2() and
+          boxes_coords[human_id][5] <= stop_points.getY3() and boxes_coords[human_id][5] <= stop_points.getY4())
          or
-         (boxes_coords[i][4] >= stop_points.getX1() and boxes_coords[i][4] <= stop_points.getX2() and
-          boxes_coords[i][4] <= stop_points.getX3() and boxes_coords[i][4] >= stop_points.getX4() and
-          boxes_coords[i][5] >= stop_points.getY1() and boxes_coords[i][5] >= stop_points.getY2() and
-          boxes_coords[i][5] <= stop_points.getY3() and boxes_coords[i][5] <= stop_points.getY4()))
+         (boxes_coords[human_id][4] >= stop_points.getX1() and boxes_coords[human_id][4] <= stop_points.getX2() and
+          boxes_coords[human_id][4] <= stop_points.getX3() and boxes_coords[human_id][4] >= stop_points.getX4() and
+          boxes_coords[human_id][5] >= stop_points.getY1() and boxes_coords[human_id][5] >= stop_points.getY2() and
+          boxes_coords[human_id][5] <= stop_points.getY3() and boxes_coords[human_id][5] <= stop_points.getY4()))
                 or  # Разграничение ИЛИ для двух точек | # Для xy второй координаты ног
-        ((boxes_coords[i][6] <= stop_points.getX1() and boxes_coords[i][6] <= stop_points.getX2() and
-          boxes_coords[i][6] <= stop_points.getX3() and boxes_coords[i][6] >= stop_points.getX4() and
-          boxes_coords[i][7] >= stop_points.getY1() and boxes_coords[i][7] >= stop_points.getY2() and
-          boxes_coords[i][7] <= stop_points.getY3() and boxes_coords[i][7] <= stop_points.getY4())
+        ((boxes_coords[human_id][6] <= stop_points.getX1() and boxes_coords[human_id][6] <= stop_points.getX2() and
+          boxes_coords[human_id][6] <= stop_points.getX3() and boxes_coords[human_id][6] >= stop_points.getX4() and
+          boxes_coords[human_id][7] >= stop_points.getY1() and boxes_coords[human_id][7] >= stop_points.getY2() and
+          boxes_coords[human_id][7] <= stop_points.getY3() and boxes_coords[human_id][7] <= stop_points.getY4())
          or
-         (boxes_coords[i][6] >= stop_points.getX1() and boxes_coords[i][6] <= stop_points.getX2() and
-          boxes_coords[i][6] >= stop_points.getX3() and boxes_coords[i][6] >= stop_points.getX4() and
-          boxes_coords[i][7] >= stop_points.getY1() and boxes_coords[i][7] >= stop_points.getY2() and
-          boxes_coords[i][7] <= stop_points.getY3() and boxes_coords[i][7] <= stop_points.getY4())
+         (boxes_coords[human_id][6] >= stop_points.getX1() and boxes_coords[human_id][6] <= stop_points.getX2() and
+          boxes_coords[human_id][6] >= stop_points.getX3() and boxes_coords[human_id][6] >= stop_points.getX4() and
+          boxes_coords[human_id][7] >= stop_points.getY1() and boxes_coords[human_id][7] >= stop_points.getY2() and
+          boxes_coords[human_id][7] <= stop_points.getY3() and boxes_coords[human_id][7] <= stop_points.getY4())
          or
-         (boxes_coords[i][6] >= stop_points.getX1() and boxes_coords[i][6] <= stop_points.getX2() and
-          boxes_coords[i][6] >= stop_points.getX3() and boxes_coords[i][6] <= stop_points.getX4() and
-          boxes_coords[i][7] >= stop_points.getY1() and boxes_coords[i][7] >= stop_points.getY2() and
-          boxes_coords[i][7] <= stop_points.getY3() and boxes_coords[i][7] <= stop_points.getY4())
+         (boxes_coords[human_id][6] >= stop_points.getX1() and boxes_coords[human_id][6] <= stop_points.getX2() and
+          boxes_coords[human_id][6] >= stop_points.getX3() and boxes_coords[human_id][6] <= stop_points.getX4() and
+          boxes_coords[human_id][7] >= stop_points.getY1() and boxes_coords[human_id][7] >= stop_points.getY2() and
+          boxes_coords[human_id][7] <= stop_points.getY3() and boxes_coords[human_id][7] <= stop_points.getY4())
          or
-         (boxes_coords[i][6] >= stop_points.getX1() and boxes_coords[i][6] <= stop_points.getX2() and
-          boxes_coords[i][6] <= stop_points.getX3() and boxes_coords[i][6] >= stop_points.getX4() and
-          boxes_coords[i][7] >= stop_points.getY1() and boxes_coords[i][7] >= stop_points.getY2() and
-          boxes_coords[i][7] <= stop_points.getY3() and boxes_coords[i][7] <= stop_points.getY4()))
+         (boxes_coords[human_id][6] >= stop_points.getX1() and boxes_coords[human_id][6] <= stop_points.getX2() and
+          boxes_coords[human_id][6] <= stop_points.getX3() and boxes_coords[human_id][6] >= stop_points.getX4() and
+          boxes_coords[human_id][7] >= stop_points.getY1() and boxes_coords[human_id][7] >= stop_points.getY2() and
+          boxes_coords[human_id][7] <= stop_points.getY3() and boxes_coords[human_id][7] <= stop_points.getY4()))
         ):
             human_inside_count += 1
-            coord_x1 = boxes_coords[i][0]
-            coord_y1 = boxes_coords[i][1]
-            coord_x4 = boxes_coords[i][6]
-            coord_y4 = boxes_coords[i][7]
+            coord_x1 = boxes_coords[human_id][0]
+            coord_y1 = boxes_coords[human_id][1]
+            coord_x4 = boxes_coords[human_id][6]
+            coord_y4 = boxes_coords[human_id][7]
             human_state_not_changed_flag = 0
             load_human(stop_id, time, coord_x1, coord_y1, coord_x4, coord_y4, human_state_not_changed_flag)
 
